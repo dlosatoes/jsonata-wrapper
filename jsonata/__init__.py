1dfcc663de7623efe89e23f199a4d92416ce58ed
Rob Meijer <rob@noemail>
2022-09-13 16:44:33 +0200

    linter stuff

--- jsonata/__init__.py
+++ jsonata/__init__.py
@@ -1,9 +1,10 @@
 """Wrapper module for the JSONATA Javascript Library"""
+import json as _json
 from _jsonata import Context as _llContext
 from _jsonata import transform as _lltransform
-import json
 
 def bigint_escape(data):
+    """Escape big integers that JavaScript can't handle"""
     rval = data
     if isinstance(data, dict):
         rval = {}
@@ -12,13 +13,14 @@ def bigint_escape(data):
     elif isinstance(data,list):
         rval = []
         for val in data:
-            rcal.append(bigint_escape(val))
+            rval.append(bigint_escape(val))
     elif isinstance(data, int):
         if data > 9007199254740991: #javascript MAX_SAFE_INT
             rval = "JSONATA-BIGINT:" + str(data)
     return rval
 
 def bigint_unescape(data):
+    """Unescape big integers that JavaScript wouldn't have been able to handle"""
     rval = data
     if isinstance(data, dict):
         rval = {}
@@ -27,13 +29,14 @@ def bigint_unescape(data):
     elif isinstance(data,list):
         rval = []
         for val in data:
-            rcal.append(bigint_unescape(val))
+            rval.append(bigint_unescape(val))
     elif isinstance(data, str):
         if data.startswith("JSONATA-BIGINT:"):
             rval = int(data.split(":")[1])
     return rval
 
 class Context:
+    # pylint: disable=too-few-public-methods
     """Reusable JSONata context for more efficient repeated transforms"""
     def __init__(self, bigint_patch=False):
         self._llcontext = _llContext()
@@ -41,11 +44,39 @@ class Context:
     def __call__(self, xform, data):
         """Do the actual transform"""
         if self.bigint_patch:
-            return bigint_unescape(json.loads(self._llcontext(xform, json.dumps(bigint_escape(data)))))
-        return json.loads(self._llcontext(xform, json.dumps(data)))
+            return bigint_unescape(
+                    _json.loads(
+                        self._llcontext(
+                            xform,
+                            _json.dumps(
+                                bigint_escape(data)
+                                )
+                            )
+                        )
+                    )
+        return _json.loads(
+                self._llcontext(
+                    xform,
+                    _json.dumps(data)
+                    )
+                )
 
 def transform(xform, data, bigint_patch=False):
     """Convenience function for one-off JSONATA transforms"""
     if bigint_patch:
-        return bigint_unescape(json.loads(_lltransform(xform, json.dumps(bigint_escape(data)))))
-    return json.loads(_lltransform(xform, json.dumps(data)))
+        return bigint_unescape(
+                _json.loads(
+                    _lltransform(
+                        xform, 
+                        _json.dumps(
+                            bigint_escape(data)
+                            )
+                        )
+                    )
+                )
+    return _json.loads(
+            _lltransform(
+                xform, 
+                _json.dumps(data)
+                )
+            )
